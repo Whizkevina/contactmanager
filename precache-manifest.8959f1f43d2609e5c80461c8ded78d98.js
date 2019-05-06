7e7aac4d1fe36192b507bc59579dbe2b8d0ab39d
Aina Tim <wizkevinaaina@gmail.com>
2019-05-06 10:39:23 +0100

    Updates

---
+++ precache-manifest.8959f1f43d2609e5c80461c8ded78d98.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "576d216f7f758eefa8c538f61c98d707",
+    "url": "/contactmanager/index.html"
+  },
+  {
+    "revision": "63c931a45b16531ef618",
+    "url": "/contactmanager/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "fb6215c05ea51f12e1d7",
+    "url": "/contactmanager/static/css/main.36497136.chunk.css"
+  },
+  {
+    "revision": "63c931a45b16531ef618",
+    "url": "/contactmanager/static/js/2.760bc175.chunk.js"
+  },
+  {
+    "revision": "fb6215c05ea51f12e1d7",
+    "url": "/contactmanager/static/js/main.f646b9c8.chunk.js"
+  },
+  {
+    "revision": "a9b80a0b0e722e0d3664",
+    "url": "/contactmanager/static/js/runtime~main.884ecd02.js"
+  }
+]);
